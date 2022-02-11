aec920fd4923aa44039dee3a56f8054e84da1aed
Vijayan Balasubramanian <balasvij@amazon.com>
2022-02-11 11:28:02 -0800

    Add uploader to upload user input
    Uploader will upload GeoJSON objects from UploadGeoJSONRequestContent as
    Documents to given index in three stage.
    At first stage (preUpload), resources like index, mapping, pipeline with
    GeoJSON Feature processors, will be created.
    At second stage (upload), Feature will be extracted from GeoJSON and indexed using
    BulkAction. This supports both Feature and FeatureCollection.
    At third stage (postUpload), previously created pipeline will be deleted and
    response will be added to the listener.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/test/java/org/opensearch/geospatial/rest/action/upload/geojson/RestUploadGeoJSONActionIT.java
+++ src/test/java/org/opensearch/geospatial/rest/action/upload/geojson/RestUploadGeoJSONActionIT.java
@@ -15,32 +15,81 @@ import static org.opensearch.geospatial.rest.action.upload.geojson.RestUploadGeo
 import static org.opensearch.geospatial.rest.action.upload.geojson.RestUploadGeoJSONAction.ACTION_UPLOAD;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.json.JSONObject;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
+import org.opensearch.client.ResponseException;
+import org.opensearch.common.settings.Settings;
 import org.opensearch.geospatial.GeospatialRestTestCase;
+import org.opensearch.geospatial.action.upload.geojson.UploadGeoJSONRequestContent;
 import org.opensearch.geospatial.plugin.GeospatialPlugin;
 import org.opensearch.rest.RestStatus;
 
 public class RestUploadGeoJSONActionIT extends GeospatialRestTestCase {
 
+    public static final int NUMBER_OF_FEATURES_TO_ADD = 3;
+
     public void testGeoJSONUploadSuccessPostMethod() throws IOException {
 
         String path = String.join(GeospatialPlugin.URL_DELIMITER, GeospatialPlugin.getPluginURLPrefix(), ACTION_OBJECT, ACTION_UPLOAD);
         Request request = new Request("POST", path);
-        request.setJsonEntity(buildUploadGeoJSONRequestContent().toString());
+        final JSONObject requestBody = buildUploadGeoJSONRequestContent(NUMBER_OF_FEATURES_TO_ADD, null);
+        final String index = requestBody.getString(UploadGeoJSONRequestContent.FIELD_INDEX.getPreferredName());
+        assertIndexNotExists(index);
+        request.setJsonEntity(requestBody.toString());
         Response response = client().performRequest(request);
         assertEquals(RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+        assertIndexExists(index);
+        assertEquals("failed to index documents", NUMBER_OF_FEATURES_TO_ADD, getIndexDocumentCount(index));
+    }
 
+    public void testGeoJSONUploadFailIndexExists() throws IOException {
+
+        Map<String, String> geoFields = new HashMap<>();
+        geoFields.put("location", "geo_shape");
+        String index = randomLowerCaseString();
+        createIndex(index, Settings.EMPTY, geoFields);
+        String path = String.join(GeospatialPlugin.URL_DELIMITER, GeospatialPlugin.getPluginURLPrefix(), ACTION_OBJECT, ACTION_UPLOAD);
+        Request request = new Request("POST", path);
+        final JSONObject requestBody = buildUploadGeoJSONRequestContent(NUMBER_OF_FEATURES_TO_ADD, index);
+        request.setJsonEntity(requestBody.toString());
+        final ResponseException responseException = assertThrows(ResponseException.class, () -> client().performRequest(request));
+        assertTrue("Not an expected exception", responseException.getMessage().contains("resource_already_exists_exception"));
     }
 
     public void testGeoJSONUploadSuccessPutMethod() throws IOException {
 
         String path = String.join(GeospatialPlugin.URL_DELIMITER, GeospatialPlugin.getPluginURLPrefix(), ACTION_OBJECT, ACTION_UPLOAD);
         Request request = new Request("PUT", path);
-        request.setJsonEntity(buildUploadGeoJSONRequestContent().toString());
+        final JSONObject requestBody = buildUploadGeoJSONRequestContent(NUMBER_OF_FEATURES_TO_ADD, null);
+        final String index = requestBody.getString(UploadGeoJSONRequestContent.FIELD_INDEX.getPreferredName());
+        assertIndexNotExists(index);
+        request.setJsonEntity(requestBody.toString());
         Response response = client().performRequest(request);
         assertEquals(RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+        assertIndexExists(index);
+        assertEquals("failed to index documents", NUMBER_OF_FEATURES_TO_ADD, getIndexDocumentCount(index));
+    }
 
+    public void testGeoJSONPutMethodUploadIndexExists() throws IOException {
+
+        Map<String, String> geoFields = new HashMap<>();
+        geoFields.put("location", "geo_shape");
+        String index = randomLowerCaseString();
+        createIndex(index, Settings.EMPTY, geoFields);
+        String path = String.join(GeospatialPlugin.URL_DELIMITER, GeospatialPlugin.getPluginURLPrefix(), ACTION_OBJECT, ACTION_UPLOAD);
+        Request request = new Request("PUT", path);
+        final JSONObject requestBody = buildUploadGeoJSONRequestContent(NUMBER_OF_FEATURES_TO_ADD, index);
+        request.setJsonEntity(requestBody.toString());
+        assertIndexExists(index);
+        int indexDocCountBeforeUpload = getIndexDocumentCount(index);
+        request.setJsonEntity(requestBody.toString());
+        Response response = client().performRequest(request);
+        assertEquals(RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+        int expectedDocCountAfterUpload = indexDocCountBeforeUpload + NUMBER_OF_FEATURES_TO_ADD;
+        assertEquals("failed to index documents", expectedDocCountAfterUpload, getIndexDocumentCount(index));
     }
 }
