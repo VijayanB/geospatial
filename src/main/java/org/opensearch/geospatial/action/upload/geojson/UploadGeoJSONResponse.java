01aa1c129a911f557542c3ab867e00c0e4980167
Vijayan Balasubramanian <balasvij@amazon.com>
2022-04-21 16:47:20 -0700

    Update Upload GeoJSON API Response
    Include following information about upload API results
    1. errors, with values either true or false
    2. total, with values as total number of documents
    3. success, with values as number of documents that are indexed
    4. failure, with values as number of documents that are failed
    5. messages, json array, with list of failure messages.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

---
+++ src/main/java/org/opensearch/geospatial/action/upload/geojson/UploadGeoJSONResponse.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.geospatial.action.upload.geojson;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.opensearch.action.ActionResponse;
+import org.opensearch.action.bulk.BulkItemResponse;
+import org.opensearch.action.bulk.BulkResponse;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+import org.opensearch.common.xcontent.ToXContentObject;
+import org.opensearch.common.xcontent.XContentBuilder;
+
+//UploadGeoJSONResponse represents UploadGeoJSONRequest's Response
+public class UploadGeoJSONResponse extends ActionResponse implements ToXContentObject {
+    private static final String ERRORS = "errors";
+    private static final String FAILURE = "failure";
+    private static final String ID = "id";
+    private static final String MESSAGES = "messages";
+    private static final String REASON = "reason";
+    private static final String SUCCESS = "success";
+    private static final String TOOK_IN_MILLI_SECOND = "took_in_ms";
+    private static final String TOTAL = "total";
+    private static final int NO_FAILURE = 0;
+    private final BulkResponse response;
+
+    public UploadGeoJSONResponse(BulkResponse response) {
+        this.response = response;
+    }
+
+    public UploadGeoJSONResponse(StreamInput in) throws IOException {
+        super(in);
+        this.response = new BulkResponse(in);
+    }
+
+    @Override
+    public void writeTo(StreamOutput streamOutput) throws IOException {
+        this.response.writeTo(streamOutput);
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        /*
+        If BulkResponse has no failures:
+            {
+              "took_in_ms": 100,
+              "errors": false,
+              "total": 5,
+              "success": 5,
+              "failure": 0
+            }
+        If BulkResponse has failures:
+            {
+              "took_in_ms": 100,
+              "errors": true,
+              "total": 4,
+              "success": 3,
+              "failure": 1,
+              "messages": [
+                  {
+                    "id" : "DocId2",
+                    "reason" : "failed to index due to ..."
+                  },
+                  {
+                    "id" : "DocId3",
+                    "reason" : "failed to index due to ..."
+                  }
+             ]
+          }
+         */
+        builder.startObject();
+        builder.field(TOOK_IN_MILLI_SECOND, response.getTook().getMillis());
+        builder.field(ERRORS, response.hasFailures());
+        builder.field(TOTAL, response.getItems().length);
+        if (!response.hasFailures()) {
+            buildSuccessXContent(builder);
+            return builder.endObject();
+        }
+        buildFailureXContent(builder);
+        return builder.endObject();
+    }
+
+    private void buildSuccessXContent(XContentBuilder builder) throws IOException {
+        buildResultXContent(builder, response.getItems().length, NO_FAILURE);
+    }
+
+    private void buildResultXContent(XContentBuilder builder, int successCount, int failureCount) throws IOException {
+        builder.field(SUCCESS, successCount);
+        builder.field(FAILURE, failureCount);
+    }
+
+    private void buildFailureXContent(XContentBuilder builder) throws IOException {
+        final Map<String, String> failedResponses = Arrays.stream(response.getItems())
+            .filter(BulkItemResponse::isFailed)
+            .collect(Collectors.toMap(BulkItemResponse::getId, BulkItemResponse::getFailureMessage));
+        int successCount = response.getItems().length - failedResponses.size();
+        buildResultXContent(builder, successCount, failedResponses.size());
+        builder.startArray(MESSAGES);
+        for (Map.Entry<String, String> entry : failedResponses.entrySet()) {
+            builder.startObject();
+            builder.field(ID, entry.getKey());
+            builder.field(REASON, entry.getValue());
+            builder.endObject();
+        }
+        builder.endArray();
+    }
+}
